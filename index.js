6e044213aa10e23889b25383a61d491d2bb210b2
Crix Rosales <crimiro@MacBook-Pro-de-Crix.local>
2019-12-12 21:03:32 -0600

    Push changes

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const app = express();
 const { config } = require('./config/index');
 const moviesAPI = require('./routes/movies');
 
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
 app.use(express.json());
 
 moviesAPI(app);
+// Catch 404
+app.use(notFoundHandler);
+// Siempre al final
+// Errors Middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
--- routes/movies.js
+++ routes/movies.js
@@ -11,6 +11,7 @@ function moviesAPI(app) {
     const { tags } = req.query;
     try {
       const movies = await moviesService.getMovies({ tags });
+      throw new Error('Error getting movies');
       res.status(200).json({
         data: movies,
         message: 'Movie Retrieved'
